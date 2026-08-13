a369633acf7e55b790f93d7acf9eea5e0ad977ba
PrathamSuresh <prathamsuresh2005@gmail.com>
2026-08-14 02:17:14 +0530

    Time: 2 ms (98.22%), Space: 11.7 MB (47.34%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.c
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.c
@@ -1,40 +1,51 @@
+int leftFinder(int *weights, int weightsSize){
+    int max=0;
+    for(int i=0;i<weightsSize;i++){
+        if(weights[i]>max){
+            max=weights[i];
+        }
+    }
+    return max;
+}
+
+int rightFinder(int *weights, int weightsSize){
+    int sum=0;
+    for(int i=0;i<weightsSize;i++){
+        sum+=weights[i];
+    }
+    return sum;
+}
+
+int DayFinder(int *weights,int weightsSize, int mid){
+    int ans=0;
+    int days=1;
+    for(int i=0;i<weightsSize;i++){
+        if(ans+weights[i]<=mid){
+            ans+=weights[i];
+        }else{
+            days++;
+            ans=weights[i];
+        }
+    }
+    return days;
+}
+
 int shipWithinDays(int* weights, int weightsSize, int days) {
-    int low = 0;
-    int high = 0;
-
-    // Minimum capacity = heaviest package
-    // Maximum capacity = sum of all packages
-    for (int i = 0; i < weightsSize; i++) {
-        if (weights[i] > low)
-            low = weights[i];
-
-        high += weights[i];
-    }
-
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        int currentWeight = 0;
-        int requiredDays = 1;
-
-        for (int i = 0; i < weightsSize; i++) {
-            if (currentWeight + weights[i] <= mid) {
-                currentWeight += weights[i];
-            } else {
-                requiredDays++;
-                currentWeight = weights[i];
-            }
+    int left=leftFinder(weights,weightsSize);
+    int right=rightFinder(weights,weightsSize);
+    int mid;
+    int check;
+    int ans=0;
+    while(left<=right){
+        mid=(left+right)/2;
+        check=DayFinder(weights,weightsSize,mid);
+        if(check<=days){
+            ans=mid;
+            right=mid-1;
+        }else{
+            left=mid+1;
         }
 
-        if (requiredDays <= days) {
-            // This capacity works.
-            // Try to find a smaller one.
-            high = mid - 1;
-        } else {
-            // Capacity is too small.
-            low = mid + 1;
-        }
     }
-
-    return low;
+    return ans;
 }
