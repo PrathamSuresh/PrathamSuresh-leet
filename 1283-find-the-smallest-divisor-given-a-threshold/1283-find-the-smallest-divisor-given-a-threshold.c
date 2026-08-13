abbc83541dea1b9c09a54b205a7463be3cce017b
PrathamSuresh <prathamsuresh2005@gmail.com>
2026-08-13 21:41:54 +0530

    Time: 2 ms (82.76%), Space: 11.4 MB (56.32%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.c
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.c
@@ -1,28 +1,38 @@
-int smallestDivisor(int* nums, int numsSize, int threshold) {
-    int low = 1;
-    int high = 0;
-
-    // Find maximum element
+int maxNum(int* nums, int numsSize) {
+    int max = 0;
     for (int i = 0; i < numsSize; i++) {
-        if (nums[i] > high)
-            high = nums[i];
-    }
-
-    // Binary search
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        int sum = 0;
-
-        for (int i = 0; i < numsSize; i++) {
-            sum += (nums[i] + mid - 1) / mid;  // ceil(nums[i] / mid)
+        if (nums[i] > max) {
+            max = nums[i];
         }
+    }
+    return max;
+}
 
-        if (sum <= threshold) {
-            high = mid - 1;   // Try a smaller divisor
+int check(int* nums, int numsSize, int mid) {
+    int ans = 0;
+    for (int i = 0; i < numsSize; i++) {
+        ans += (nums[i] + mid - 1) / mid;
+    }
+    return ans;
+}
+
+int smallestDivisor(int* nums, int numsSize, int threshold) {
+    int max = maxNum(nums, numsSize);
+    int left = 1;
+    int right = max;
+    int mid;
+    int ans = 0;
+    int final = 0;
+    while (left <= right) {
+        ans = 0;
+        mid = (left + right) / 2;
+        ans = check(nums, numsSize, mid);
+        if (ans <= threshold) {
+            final = mid;
+            right=mid-1;
         } else {
-            low = mid + 1;    // Need a larger divisor
+            left=mid+1;
         }
     }
-
-    return low;
+    return final;
 }
