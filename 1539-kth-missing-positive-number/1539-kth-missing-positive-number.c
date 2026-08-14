f8583fd36d3384ec2e4c61e84f75176dc2c90750
PrathamSuresh <prathamsuresh2005@gmail.com>
2026-08-14 14:16:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (55.77%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.c
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.c
@@ -3,13 +3,15 @@ int findKthPositive(int* arr, int arrSize, int k) {
     int final=0;
     int r=0;
     for(int i=0;i<arrSize;i++){
-        ans=arr[i]-(i+1); 
+        ans=arr[i]-(i+1);
         if(ans>=k){
             r=k+i;
             return r;
+        }else if(ans<k){
+            final=k-ans;
+            r=arr[i]+final;
         }
-       
     }
-    return k+arrSize;
+    return r;
 
 }
