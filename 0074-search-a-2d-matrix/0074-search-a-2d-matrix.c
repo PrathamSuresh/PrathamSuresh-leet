f83c1a56e9fcafc9f683ca9824a61e414f68650e
PrathamSuresh <prathamsuresh2005@gmail.com>
2026-08-15 00:11:38 +0530

    Time: 0 ms (100%), Space: 9.5 MB (57.78%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
@@ -0,0 +1,18 @@
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    int left=0;
+    int right=matrixSize*matrixColSize[0]-1;
+    int mid;
+    while(left<=right){
+        mid=(left+right)/2;
+        int row=mid/matrixColSize[0];
+        int column=mid%matrixColSize[0];
+        if(matrix[row][column]==target){
+            return true;
+        }else if(matrix[row][column]<target){
+            left=mid+1;
+        }else{
+            right=mid-1;
+        }
+    }
+    return false;
+}
